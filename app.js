49d3f46232c7dcf59a994a70134704a402fb9ac9
Tushar Saini <s2sharpit@gmail.com>
2023-04-03 01:13:26 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,49 +1,36 @@
 import express from 'express';
-import mongoose from 'mongoose';
+import userRouter from './routes/user.js';
+import taskRouter from './routes/task.js';
+import {config} from "dotenv";
+import cookieParser from 'cookie-parser';
+import { errorMiddleware } from './middlewares/error.js';
+import cors from "cors";
 
-const app = express();
+export const app = express();
 
-mongoose
-    .connect('mongodb://127.0.0.1:27017', {
-        dbName: "backendapi",
-    })
-    .then(() => console.log("Database Connected"))
-    .catch((e) => console.log(e));
-
-const schema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
+config({
+    path: "./data/config.env"
 });
 
-const User = mongoose.model("User", schema);
+const router = express.Router();
+
+// Using Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
+
+// Using Routes
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/task", taskRouter);
+
 
 app.get("/", (req, res) => {
     res.send("Nice Working.");
 });
 
-app.get("/users/all", async (req, res) => {
-    const users = await User.find({});
-
-    res.json({
-        success: true,
-        users: [],
-    });
-});
-
-app.post("/users/new", async (req, res) => {
-    await User.create({
-        name: "Tushar",
-        email: "s2sharpit@gmail.com",
-        password: "s2sharpit",
-    });
-
-    res.json({
-        success: true,
-        message: "Registered Successfully",
-    });
-});
-
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-});
+// Using Error Middleware
+app.use(errorMiddleware);
